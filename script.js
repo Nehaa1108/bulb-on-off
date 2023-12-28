73dc2168bc556fd6bf9d065fb753ec6d7b1e3e39
Neha Gupta <127198995+Nehaa1108@users.noreply.github.com>
2023-12-28 19:02:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//turn on function
+function bulbon(){
+    let bulbonn=document.querySelector(".light");
+    bulbonn.setAttribute("src","lighton.png");
+}
+//turn off function
+function bulboff(){
+    let bulboff=document.querySelector(".light");
+    bulboff.setAttribute("src","lightoff.png");
+}
